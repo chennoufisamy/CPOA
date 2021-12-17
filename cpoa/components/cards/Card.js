93f1b90ab82ac7cd5f77a35b5413e9186678f12a
MiTiX <bastien.boulet@etu.univ-lyon1.fr>
2021-12-18 00:46:22 +0100

    random colors for cards

--- cpoa/components/cards/Card.js
+++ cpoa/components/cards/Card.js
@@ -2,11 +2,25 @@ import styles from "./Card.module.css";
 import Link from "next/link"
 
 const Card = ({ title, button_title, link }) => {
+
+    const colors = {
+        green: {bg: "#C9F4BE", border: "#75F128"},
+        blue: {bg: "#ace4e7", border: "#4bc3cb"},
+        beige: {bg: "#fdefda", border: "#f8d49f"},
+        pink: {bg: "#fddada", border: "#f89f9f"},
+        purple: {bg: "#e9dafd", border: "#ba8bf6"}
+    }
+
+    var keys = Object.keys(colors)
+    var randomKey = keys[Math.floor(Math.random() * keys.length)]
+
+    let color = colors[randomKey]
+
     return (
-        <div className={styles.container}>
+        <div style={{backgroundColor: color.bg}} className={styles.container}>
             <h3 className={styles.title}>{title}</h3>
             <Link href={link}>
-                <a className={styles.button}>{button_title}</a>
+                <a style={{border: `2px solid ${color.border}`}} className={styles.button}>{button_title}</a>
             </Link>
         </div>
     )
--- cpoa/components/cards/CardsList.js
+++ cpoa/components/cards/CardsList.js
@@ -2,10 +2,16 @@ import Card from "./card";
 import styles from "./Card.module.css"
 
 const CardsList = ({ cards }) => {
+
     return (
         <div className={styles.list}>
             {cards.map(f =>
-                <Card key={f.title} title={f.title} button_title={f.button_title} link={f.link}color={f.color}/>
+                <Card 
+                    key={f.title} 
+                    title={f.title} 
+                    button_title={f.button_title} 
+                    link={f.link} 
+                />
             )}
         </div>
     )
